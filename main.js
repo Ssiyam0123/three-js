16ec59b8a239aa504d36a9d98f63a7080a8ee591
Ssiyam0123 <ssiyam563@gmail.com>
2025-03-02 23:53:51 +0600

    plane mesh

--- main.js
+++ main.js
@@ -16,11 +16,16 @@ document.body.appendChild(renderer.domElement);
 
 // Create a cube
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 }); // Corrected color
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 }); 
 const mesh = new THREE.Mesh(boxGeometry, material);
-
 scene.add(mesh);
 camera.position.z = 5;
+
+const PlaneGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
+const planeMaterial = new THREE.MeshBasicMaterial({color: 0xff0000})
+const planeMesh = new THREE.Mesh(PlaneGeometry, planeMaterial)
+scene.add(planeMesh)
+
 
 function animate() {
   requestAnimationFrame(animate);
@@ -28,5 +33,8 @@ function animate() {
   mesh.rotation.x += 0.01;
   mesh.rotation.y += 0.01;
   mesh.rotation.z += 0.01;
+
+  planeMesh.rotation.x += 0.01
+  
 }
 animate();
